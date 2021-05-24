db2777c0a162dbfe665bf0e8113ced694c645a5c
derekpinto2k <dpinto@uoregon.edu>
2021-05-24 14:30:03 -0700

    no message

--- brevets/tests/test_db.py
+++ brevets/tests/test_db.py
@@ -1,17 +1,18 @@
 import os
 from pymongo import MongoClient
-import arrow
+
 
 client = MongoClient('mongodb://' + os.environ['MONGODB_HOSTNAME'], 27017)
 db = client.brevetsdb
 
 def test_db_insert_and_retrieve():
-    times_insert = {'open': arrow.get(2021).format('YYYY-MM-DDTHH:mm'),
-                    'close': arrow.get(2022).format('YYYY-MM-DDTHH:mm')}
+    times_insert = [{'open': '2021-01-01T05:53', 'close': '2021-01-01T13:20'},
+                    {'open': '2021-01-02T03:44', 'close': '2021-01-03T13:53'}]
+
+    print(times_insert)
     db.times.drop()
     for item in times_insert:
         db.times_insert.insert_one(item)
 
     times_retrieve = list(db.times_insert.find())
     assert times_retrieve
-
